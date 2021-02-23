d956d28de115588500f9132848c4fc0b94bc47c0
Владимир Бебишев <vladimirbebishev@Gmail.com>
2021-02-23 13:01:07 +1000

    delete main

--- hzz/Source.cpp
+++ hzz/Source.cpp
@@ -3,27 +3,6 @@
 #include <string>
 using namespace std;
 int main() {
-    string s;
-    getline(cin, s);
-    int max = 0, pos = 0, now = 0;
-    s += ' ';
-    int y = s.length();
-    
-    for (int i = 0; i < y; i++) {
-        if (s[i] != ' ') {
-            now++;
-        }
-        else {
-            if (now > max) {
-                max = now;
-                pos = i - now;
-            }
-            now = 0;
-        }
-    }
-    for (int i = pos; i <= pos + max; i++) {
-        cout << s[i];
-    }
-    return 0;
+
 }
 
